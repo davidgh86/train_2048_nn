4544fd669b42677e24cf4cae398ae3564cf1555b
david <jgarcia.ext@aenor.com>
2020-01-10 14:11:47 +0100

    random forest

--- 2048_random_forest.py
+++ 2048_random_forest.py
@@ -20,22 +20,21 @@ moves_model = DecisionTreeRegressor(random_state=1)
 # Fit Model
 moves_model.fit(train_X, train_y)
 
-# Make validation predictions and calculate mean absolute error
-val_predictions = moves_model.predict(val_X)
-val_mae = mean_absolute_error(val_predictions, val_y)
-print("Validation MAE when not specifying max_leaf_nodes: {:,.0f}".format(val_mae))
 
-# Using best value for max_leaf_nodes
-moves_model = DecisionTreeRegressor(max_leaf_nodes=100, random_state=1)
-moves_model.fit(train_X, train_y)
-val_predictions = moves_model.predict(val_X)
-val_mae = mean_absolute_error(val_predictions, val_y)
-print("Validation MAE for best value of max_leaf_nodes: {:,.0f}".format(val_mae))
+def getWrongValues(results, validation_values):
+    result = 0
+    for i in range(len(results)):
+        if results[i] != validation_values.iloc[i]:
+            result += 1
+    return result
+
 
 # Define the model. Set random_state to 1
 rf_model = RandomForestRegressor(random_state=1)
 rf_model.fit(train_X, train_y)
 rf_val_predictions = rf_model.predict(val_X)
-rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
+rf_val_predictions = list(map(lambda x: int(round(x)), rf_val_predictions))
+error = getWrongValues(rf_val_predictions, val_y)/len(val_y)
 
-print("Validation MAE for Random Forest Model: {:,.0f}".format(rf_val_mae))
+
+print("error: ", error)
